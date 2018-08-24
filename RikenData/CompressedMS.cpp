14bfa17f032d9cac2c458b605b4851ceddbbda6a
Aleksei Chudinov <chudinov@inbox.ru>
2018-08-24 03:01:53 +0300

    First version of integer interpolator was debugged

---
+++ RikenData/CompressedMS.cpp
@@ -0,0 +1,5 @@
+#include "CompressedMS.h"
+
+CompressedMS::CompressedMS()
+{
+}
---
+++ RikenData/CompressedMS.h
@@ -0,0 +1,10 @@
+#ifndef COMPRESSEDMS_H
+#define COMPRESSEDMS_H
+
+class CompressedMS
+{
+public:
+    CompressedMS();
+};
+
+#endif // COMPRESSEDMS_H
