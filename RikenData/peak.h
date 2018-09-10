b623496f48088bf2ba2822d15314d13d34e2e202
AlexChudinov <chudinov@inbox.ru>
2018-09-10 05:54:04 +0300

    Peak had some small refactoring

--- RikenData/peak.h
+++ RikenData/peak.h
@@ -14,25 +14,25 @@ class Peak
     double m_height;
 
     //Keeps the peak position uncertainty
-    double m_disp;
+    mutable double m_disp;
 public:
 
     using PeakCollection = std::set<Peak>;
 
-    Peak
-    (
-            double center = 0.0,
-            double left = 0.0,
-            double right = 0.0,
-            double height = 0.0,
-            double disp = 0.0
-            ) :
-          m_left(left),
-          m_right(right),
-          m_center(center),
-          m_height(height),
-          m_disp(disp)
-    {}
+	Peak
+	(
+		double center = 0.0,
+		double left = 0.0,
+		double right = 0.0,
+		double height = 0.0,
+		double disp = 0.0
+	) :
+		m_left(left),
+		m_right(right),
+		m_center(center),
+		m_height(height),
+		m_disp(disp)
+	{}
 
     double left() const;
     void setLeft(double left);
@@ -56,18 +56,22 @@ public:
      * @param pr
      * @return
      */
-    inline bool operator < (const Peak& pr) const { return m_center < pr.center(); }
+    inline bool operator < (const Peak& pr) const 
+	{ 
+		return m_center < pr.center(); 
+	}
 
     /**
      * @brief parabolicMaximum calculates maxima position and height
      * using three points
      */
-    void parabolicMaximum(
-            double x1,
-            double y0,
-            double y1,
-            double y2
-            );
+	void parabolicMaximum
+	(
+		double x1,
+		double y0,
+		double y1,
+		double y2
+	);
 };
 
 #endif // PEAK_H
